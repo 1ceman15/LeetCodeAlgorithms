0a782e5fb880f644669b1d11021ce2eb01c9abc3
Muradly Muhammed <mmuradly2@gmail.com>
2024-09-20 17:46:37 +0300

    Merge branch 'master' of https://github.com/1ceman15/LeetCodeAlgorithms

---
+++ src/Permutations.java
@@ -0,0 +1,38 @@
+
+//https://leetcode.com/problems/permutations/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+
+        helper(0, nums);
+        return result;
+    }
+
+    private void helper(int i, int[] nums){
+        if(i == nums.length){
+            List<Integer> perm = new ArrayList<>();
+            for(int num: nums){
+                perm.add(num);
+            }
+            result.add(perm);
+            return;
+        }
+
+        for(int idx = i; idx< nums.length; idx++){
+            swap(idx,i,nums);
+            helper(i+1, nums);
+            swap(idx,i,nums);
+        }
+        return;
+    }
+    private void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
---
+++ src/Subsets2.java
@@ -0,0 +1,44 @@
+
+//https://leetcode.com/problems/subsets-ii/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Subsets2 {
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+        Arrays.sort(nums);
+
+        List<Integer> list = new ArrayList<>();
+
+        backtracking(nums, 0, list);
+
+
+        return result;
+    }
+
+
+    public void backtracking(int[] nums, int index, List<Integer> list) {
+
+        if (index >= nums.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        list.add(nums[index]);
+        backtracking(nums, index + 1, list);
+        list.remove(list.size() - 1);
+
+        while (index + 1 < nums.length && nums[index] == nums[index + 1])
+            index++;
+
+        backtracking(nums, index + 1, list);
+
+    }
+
+
+}
