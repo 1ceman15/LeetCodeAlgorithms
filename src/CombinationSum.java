0b4bb3ae3056fddfb16e1b3698a9d58e4481ec84
Muradly Muhammed <mmuradly2@gmail.com>
2024-09-22 14:10:15 +0300

    Added new  solutions

---
+++ src/CombinationSum.java
@@ -0,0 +1,43 @@
+
+//https://leetcode.com/problems/combination-sum/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        backtracking(0,target,0,new ArrayList<>(),candidates);
+
+        return result;
+    }
+
+    public void backtracking(int total, int target,  int index, List<Integer> list, int[] candidates){
+        if(total==target){
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        if(total>target || index >= candidates.length)
+            return;
+
+        backtracking(total,target,index+1,list,candidates);
+
+        list.add(candidates[index]);
+        backtracking(total+candidates[index], target, index, list, candidates);
+        list.remove(list.size()-1);
+
+
+    }
+
+    public static void main(String[] args) {
+        CombinationSum c = new CombinationSum();
+
+        c.combinationSum(new int[]{2,3,6,7},7);
+        System.out.println(c.result);
+    }
+
+
+
+}
