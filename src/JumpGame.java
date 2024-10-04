1d211f90296cd8218f1aa5c9263fdc43660572e9
Muradly Muhammed <mmuradly2@gmail.com>
2024-10-04 19:53:36 +0300

    Added new  solutions

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -3,20 +3,16 @@
 
 public class JumpGame {
     public boolean canJump(int[] nums) {
-        int[] dp = new int[nums.length];
-        dp[0] = 1;
-        for (int i = 0; i < nums.length; i++) {
-            if(dp[i] != 0){
-                for (int j = i+1; j <= i + nums[i] && j < nums.length; j++) {
-                    dp[j]++;
-                }
-            }
+        int pointer = nums.length - 1;
 
+        for (int i = nums.length-1; i >= 0; i--) {
+            if(nums[i] + i >= pointer)
+                pointer = i;
         }
 
 
 
-        return dp[nums.length - 1] != 0;
+        return pointer == 0;
 
     }
 }
---
+++ src/NumberOfLongestIncreasingSubsequence.java
@@ -0,0 +1,35 @@
+
+//https://leetcode.com/problems/number-of-longest-increasing-subsequence/description/
+
+
+import java.util.Arrays;
+
+public class NumberOfLongestIncreasingSubsequence {
+
+
+    public int findNumberOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int[] count = new int[nums.length];
+        Arrays.fill(dp,1);
+
+        int maxLength = 0;
+        int res = 0;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int len = 1;
+            int cnt = 1;
+            for (int j = i; j < nums.length; j++) {
+                if(nums[i]<nums[j]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+
+                }
+
+
+            }
+
+        }
+
+
+        return res;
+    }
+}
